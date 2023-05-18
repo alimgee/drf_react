ea0b8d17d4743ca8ebf17e39b84eb76e5a980d27
alimgee <mcgee.al@gmail.com>
2023-05-18 14:00:06 +0000

    Adding Header and Footer using MaterialUI

--- react/blogapi/src/components/Footer.js
+++ react/blogapi/src/components/Footer.js
@@ -1,7 +1,80 @@
-function Footer() {
+import React from 'react';
 
+import {Container, Grid, Typography, Box, Link} from '@mui/material';
+
+
+
+function Copyright() {
 	return (
-			<h1>footer</h1>
+		<Typography variant="body2" color="textSecondary" align="center">
+			{'Copyright © '}
+			<Link color="inherit" href="https://material-ui.com/">
+				Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
 	);
 }
+
+const footers = [
+	{
+		title: 'Company',
+		description: ['Team', 'History', 'Contact us', 'Locations'],
+	},
+	{
+		title: 'Features',
+		description: [
+			'Cool stuff',
+			'Random feature',
+			'Team feature',
+			'Developer stuff',
+			'Another one',
+		],
+	},
+	{
+		title: 'Resources',
+		description: [
+			'Resource',
+			'Resource name',
+			'Another resource',
+			'Final resource',
+		],
+	},
+	{
+		title: 'Legal',
+		description: ['Privacy policy', 'Terms of use'],
+	},
+];
+
+function Footer() {
+	return (
+		<React.Fragment>
+			<Container maxWidth="md" component="footer" >
+				<Grid container spacing={4} justify="space-evenly">
+					{footers.map((footer) => (
+						<Grid item xs={6} sm={3} key={footer.title}>
+							<Typography variant="h6" color="textPrimary" gutterBottom>
+								{footer.title}
+							</Typography>
+							<ul>
+								{footer.description.map((item) => (
+									<li key={item}>
+										<Link href="#" variant="subtitle1" color="textSecondary">
+											{item}
+										</Link>
+									</li>
+								))}
+							</ul>
+						</Grid>
+					))}
+				</Grid>
+				<Box mt={5}>
+					<Copyright />
+				</Box>
+			</Container>
+		</React.Fragment>
+	);
+}
+
 export default Footer;
--- react/blogapi/src/components/Header.js
+++ react/blogapi/src/components/Header.js
@@ -1,7 +1,21 @@
-function Header() {
+import React from 'react';
+import {AppBar, Toolbar, Typography, CssBaseline } from '@mui/material';
 
+
+
+function Header() {
 	return (
-			<h1>Header</h1>
+		<React.Fragment>
+			<CssBaseline />
+			<AppBar position='static' >
+				<Toolbar>
+					<Typography variant="h6" color="inherit" noWrap>
+						BlogmeUp
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</React.Fragment>
 	);
 }
+
 export default Header;
