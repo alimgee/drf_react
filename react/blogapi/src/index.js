8a76492cc594d195045094353e64ab645b9a3cf3
alimgee <mcgee.al@gmail.com>
2023-05-18 11:29:00 +0000

    Adding root route to react app

--- react/blogapi/src/index.js
+++ react/blogapi/src/index.js
@@ -5,14 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
+
   <React.StrictMode>
-    <Header />
-    <App/>
-    <Footer />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+      <Footer />
+    </Router>
   </React.StrictMode>
 );
 
