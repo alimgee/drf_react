706cacefca2fca6826f25195731d69cb6b9f6f0f
alimgee <>
2023-06-22 08:46:28 +0000

    installinging simple jwt

--- drf_api/core/settings.py
+++ drf_api/core/settings.py
@@ -142,7 +142,10 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 CORS_ALLOWED_ORIGINS = [
